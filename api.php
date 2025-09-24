7fe97c98ba781c6a3a28cf6335ad6fc7ec5ede96
carlson <carlsonsentery@gmail.com>
2025-09-24 15:50:10 +0800

    文件名全小写，规避linux服务器上大小写问题

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
 
-require_once __DIR__ . "/services/UserService.php";
+require_once __DIR__ . "/services/userService.php";
 
 $account = $_REQUEST["account"] ?? null;
 if (!$account) {

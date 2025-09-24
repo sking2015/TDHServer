9e3e98c6dfe44cbdefdf2667d9ef5e930a257b2b
carlson <carlsonsentery@gmail.com>
2025-09-24 18:33:39 +0800

    加上请求头，方便调试程序

--- api.php
+++ api.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:7456");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
 header("Content-Type: application/json; charset=utf-8");
 
 require_once __DIR__ . "/services/userservice.php";
@@ -17,5 +22,3 @@ echo json_encode([
     "account" => $user->account,
     "userid"  => $user->userid
 ]);
-
-
